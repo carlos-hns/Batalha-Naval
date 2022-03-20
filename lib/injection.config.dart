eb792dfee1a49f31327ada224a8c7e7fbdd2e0b7
Carlos Henrique <carloshenriquend.santos@gmail.com>
2022-03-20 10:29:41 -0300

    ajustando como debugar

--- lib/injection.config.dart
+++ lib/injection.config.dart
@@ -8,8 +8,9 @@ import 'package:get_it/get_it.dart' as _i1;
 import 'package:injectable/injectable.dart' as _i2;
 
 import 'view_models/insercao_navios_view_model.dart' as _i3;
-import 'view_models/jogo_view_model.dart'
-    as _i4; // ignore_for_file: unnecessary_lambdas
+import 'view_models/jogo_view_model.dart' as _i4;
+import 'view_models/pagina_testes_view_model.dart'
+    as _i5; // ignore_for_file: unnecessary_lambdas
 
 // ignore_for_file: lines_longer_than_80_chars
 /// initializes the registration of provided dependencies inside of [GetIt]
@@ -18,5 +19,6 @@ _i1.GetIt $initGetIt(_i1.GetIt get,
   final gh = _i2.GetItHelper(get, environment, environmentFilter);
   gh.factory<_i3.InsercaoNaviosViewModel>(() => _i3.InsercaoNaviosViewModel());
   gh.factory<_i4.JogoViewModel>(() => _i4.JogoViewModel());
+  gh.factory<_i5.PaginaTestesViewModel>(() => _i5.PaginaTestesViewModel());
   return get;
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:batalha_naval/paginas/insercao_navios_page.dart';
 import 'package:batalha_naval/paginas/jogo_page.dart';
 import 'package:batalha_naval/paginas/menu_page.dart';
+import 'package:batalha_naval/paginas/pagina_testes.dart';
 import 'package:batalha_naval/tipos/tabuleiro/tabuleiro_navios.dart';
 import 'package:flutter/material.dart';
 
@@ -22,9 +23,10 @@ class MyApp extends StatelessWidget {
       title: "Batalha Naval",
       debugShowCheckedModeBanner: false,
       //home: MenuPage(),
-      home: JogoPage(
-        tabuleiroNavios: TabuleiroNavios(limiteHorizontal: 15, limiteVertical: 15),
-      ),
+      // home: JogoPage(
+      //   tabuleiroNavios: TabuleiroNavios(limiteHorizontal: 15, limiteVertical: 15),
+      // ),
+      home: PaginaTestes(),
     );
   }
 }
